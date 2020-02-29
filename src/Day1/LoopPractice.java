e5e6435437c5895b1e2793c5cdeaadf616cb3e54
zemmus77 <muhammedoff@gmail.com>
2020-02-29 15:40:15 -0500

    Add loop practice file

---
+++ src/Day1/LoopPractice.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("Counter" +i);
+
+        }
+
+    }
+}
