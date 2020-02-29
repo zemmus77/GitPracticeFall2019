250edf1163c97727bfa2db04ff60d7f272ad4178
zemmus77 <muhammedoff@gmail.com>
2020-02-29 15:53:46 -0500

    update

--- src/Day1/LoopPractice.java
+++ src/Day1/LoopPractice.java
@@ -5,6 +5,7 @@ public class LoopPractice {
         for (int i = 0; i < 10 ; i++) {
             System.out.println("Counter" +i);
             System.out.println("i = " + i);
+            System.out.println("git is fun ");
 
         }
 
