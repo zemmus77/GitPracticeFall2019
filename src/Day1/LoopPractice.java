ce06df6faadbbe6e36c621b3cadc2c72d5578154
zemmus77 <muhammedoff@gmail.com>
2020-02-29 15:50:04 -0500

    Git Practice Fall 2019

--- src/Day1/LoopPractice.java
+++ src/Day1/LoopPractice.java
@@ -4,6 +4,7 @@ public class LoopPractice {
     public static void main(String[] args) {
         for (int i = 0; i < 10 ; i++) {
             System.out.println("Counter" +i);
+            System.out.println("i = " + i);
 
         }
 
